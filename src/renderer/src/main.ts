bc9cd7a37c97e67aa34cf4f92157b8e01e9c31f7
Hong <917777904@qq.com>
2024-07-07 18:52:30 +0800

    引入图标

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,5 +1,6 @@
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import { createApp } from 'vue'
 
@@ -7,6 +8,10 @@ import App from './App.vue'
 import './style/index.js'
 
 const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(ElementPlus)
 app.mount('#app')
